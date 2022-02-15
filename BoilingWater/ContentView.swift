7a6228385f77519d5ff9c3bb9251419cfbf8d097
Judy <jyu@lcs.on.ca>
2022-02-15 12:03:49 -0500

    Add output for the static layout

--- BoilingWater/ContentView.swift
+++ BoilingWater/ContentView.swift
@@ -17,28 +17,48 @@ struct ContentView: View {
     
     var body: some View {
         VStack(spacing: 20) {
+            // Input
+            Text("Temperature at which water begins to boil (∘C):")
+                .bold()
+            
+            Group {
+                // Show the selected temperature value
+                HStack {
+                    Spacer()
+                    Text("100")
+                        .font(.title2)
+                        .bold()
+                    Spacer()
+                }
+            }
+            
             Slider(value: .constant(100.0),
                    in: 80.0...200.0,
                    step: 0.5,
                    label: {
-                        Text("Temperature at which water begins to boil (∘C):")
+                Text("Temperature at which water begins to boil (∘C):")
             },
                    minimumValueLabel: {
-                        Text("80")
+                Text("80")
             },
                    maximumValueLabel: {
-                        Text("200")
+                Text("200")
             })
             //show slider value
-            Text("")
+            Text("100")
+                .font(.title2)
+                .bold()
             
             //output
-            Text("")
+            Text("The atmospheric pressure is 0 kPa, at sea level")
             
             //some animation
             //sea
+            
             //land
+            
             //air
+            
             
             Spacer()
         }
