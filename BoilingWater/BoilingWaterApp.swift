e25c71f8eb6dda1889890ee22761cb9c1ef1c30a
Judy <jyu@lcs.on.ca>
2022-02-15 11:07:01 -0500

    Initial Commit

---
+++ BoilingWater/BoilingWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoilingWaterApp.swift
+//  BoilingWater
+//
+//  Created by Judy Yu on 2022-02-15.
+//
+
+import SwiftUI
+
+@main
+struct BoilingWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
